c2bf094d887e6801aeb5a3c310b0f0bf5e5dcabe
jeezab <zabvenen@yandex.ru>
2024-09-28 07:30:19 +0300

    refactor: added const usage

--- src/stack/stack.c
+++ src/stack/stack.c
@@ -1,43 +1,45 @@
 #include "stack.h"
 
-#define DEFINE_STACK_FUNCTIONS(TYPE, TYPE_SUFFIX)                             \
-    void initStack_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX *stack) {                \
-        stack->top = NULL;                                                    \
-    }                                                                         \
-                                                                              \
-    int isEmpty_##TYPE_SUFFIX(const Stack_##TYPE_SUFFIX *stack) {             \
-        return stack->top == NULL;                                            \
-    }                                                                         \
-                                                                              \
-    void push_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX *stack, TYPE value) {         \
-        StackNode_##TYPE_SUFFIX *newNode = (StackNode_##TYPE_SUFFIX *)malloc( \
-            sizeof(StackNode_##TYPE_SUFFIX));                                 \
-        if (!newNode) {                                                       \
-            printf("Memory allocation failed [push()]\n");                    \
-            exit(EXIT_FAILURE);                                               \
-        }                                                                     \
-        newNode->data = value;                                                \
-        newNode->next = stack->top;                                           \
-        stack->top = newNode;                                                 \
-    }                                                                         \
-                                                                              \
-    TYPE pop_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX *stack) {                      \
-        if (isEmpty_##TYPE_SUFFIX(stack)) {                                   \
-            printf("Stack is empty! [pop()]\n");                              \
-            exit(EXIT_FAILURE);                                               \
-        }                                                                     \
-        StackNode_##TYPE_SUFFIX *temp = stack->top;                           \
-        TYPE value = temp->data;                                              \
-        stack->top = temp->next;                                              \
-        free(temp);                                                           \
-        return value;                                                         \
-    }                                                                         \
-                                                                              \
-    void destroyStack_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX *stack) {             \
-        while (!isEmpty_##TYPE_SUFFIX(stack)) {                               \
-            pop_##TYPE_SUFFIX(stack);                                         \
-        }                                                                     \
+#define DEFINE_STACK_FUNCTIONS(TYPE, TYPE_SUFFIX)                       \
+    void initStack_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX *const stack) {    \
+        stack->top = NULL;                                              \
+    }                                                                   \
+                                                                        \
+    int isEmpty_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX const *const stack) { \
+        return stack->top == NULL;                                      \
+    }                                                                   \
+                                                                        \
+    void push_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX *const stack,           \
+                            TYPE const value) {                         \
+        StackNode_##TYPE_SUFFIX *const newNode =                        \
+            (StackNode_##TYPE_SUFFIX *)malloc(                          \
+                sizeof(StackNode_##TYPE_SUFFIX));                       \
+        if (!newNode) {                                                 \
+            printf("Memory allocation failed [push()]\n");              \
+            exit(EXIT_FAILURE);                                         \
+        }                                                               \
+        newNode->data = value;                                          \
+        newNode->next = stack->top;                                     \
+        stack->top = newNode;                                           \
+    }                                                                   \
+                                                                        \
+    TYPE pop_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX *const stack) {          \
+        if (isEmpty_##TYPE_SUFFIX(stack)) {                             \
+            printf("Stack is empty! [pop()]\n");                        \
+            exit(EXIT_FAILURE);                                         \
+        }                                                               \
+        StackNode_##TYPE_SUFFIX *const temp = stack->top;               \
+        TYPE const value = temp->data;                                  \
+        stack->top = temp->next;                                        \
+        free(temp);                                                     \
+        return value;                                                   \
+    }                                                                   \
+                                                                        \
+    void destroyStack_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX *const stack) { \
+        while (!isEmpty_##TYPE_SUFFIX(stack)) {                         \
+            pop_##TYPE_SUFFIX(stack);                                   \
+        }                                                               \
     }
 
-DEFINE_STACK_FUNCTIONS(int, int)
-DEFINE_STACK_FUNCTIONS(double, double)
+DEFINE_STACK_FUNCTIONS(int, int);
+DEFINE_STACK_FUNCTIONS(double, double);
--- src/stack/stack.h
+++ src/stack/stack.h
@@ -4,23 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DECLARE_STACK_TYPE(TYPE, TYPE_SUFFIX)                        \
-    typedef struct StackNode_##TYPE_SUFFIX {                         \
-        TYPE data;                                                   \
-        struct StackNode_##TYPE_SUFFIX *next;                        \
-    } StackNode_##TYPE_SUFFIX;                                       \
-                                                                     \
-    typedef struct Stack_##TYPE_SUFFIX {                             \
-        StackNode_##TYPE_SUFFIX *top;                                \
-    } Stack_##TYPE_SUFFIX;                                           \
-                                                                     \
-    void initStack_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX *stack);        \
-    int isEmpty_##TYPE_SUFFIX(const Stack_##TYPE_SUFFIX *stack);     \
-    void push_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX *stack, TYPE value); \
-    TYPE pop_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX *stack);              \
-    void destroyStack_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX *stack);
+#define DECLARE_STACK_TYPE(TYPE, TYPE_SUFFIX)                          \
+    typedef struct StackNode_##TYPE_SUFFIX {                           \
+        TYPE data;                                                     \
+        struct StackNode_##TYPE_SUFFIX *next;                          \
+    } StackNode_##TYPE_SUFFIX;                                         \
+                                                                       \
+    typedef struct Stack_##TYPE_SUFFIX {                               \
+        StackNode_##TYPE_SUFFIX *top;                                  \
+    } Stack_##TYPE_SUFFIX;                                             \
+                                                                       \
+    void initStack_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX *const stack);    \
+    int isEmpty_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX const *const stack); \
+    void push_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX *const stack,          \
+                            TYPE const value);                         \
+    TYPE pop_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX *const stack);          \
+    void destroyStack_##TYPE_SUFFIX(Stack_##TYPE_SUFFIX *const stack);
 
-DECLARE_STACK_TYPE(int, int)
-DECLARE_STACK_TYPE(double, double)
+DECLARE_STACK_TYPE(int, int);
+DECLARE_STACK_TYPE(double, double);
 
 #endif  // STACK_H
