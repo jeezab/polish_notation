679485b24b744840df106c06bdf7fa07f3ce7634
jeezab <zabvenen@yandex.ru>
2024-09-28 07:17:07 +0300

    refactor: added the use of const; errors are now output to stderr

--- src/graph/plot_graph.c
+++ src/graph/plot_graph.c
@@ -1,11 +1,12 @@
 #include "plot_graph.h"
 
-int plot_graph(const char *postfixExpression) {
-    int x_min = -20, x_max = 20;
-    int y_min = -10, y_max = 10;
+int plot_graph(char const *const postfixExpression) {
+    int const x_min = -20, x_max = 20;
+    int const y_min = -10, y_max = 10;
 
     int num_points = 0;
-    Point *points = gen_points(postfixExpression, x_min, x_max, &num_points);
+    Point *const points =
+        gen_points(postfixExpression, x_min, x_max, &num_points);
 
     for (int y = 0; y < GRAPH_HEIGHT; y++) {
         for (int x = 0; x < GRAPH_WIDTH; x++) {
@@ -22,29 +23,30 @@ int plot_graph(const char *postfixExpression) {
     return 1;
 }
 
-int evaluate_postfix_expression(const char *postfixExpression, double x_value,
-                                double *result) {
-    char *x_replaced_expr = replace_x_with_value(postfixExpression, x_value);
+int evaluate_postfix_expression(char const *const postfixExpression,
+                                double const x_value, double *const result) {
+    char *const x_replaced_expr =
+        replace_x_with_value(postfixExpression, x_value);
     if (!x_replaced_expr) {
         return -1;
     }
 
-    int status = polish_calc(x_replaced_expr, result);
+    int const status = polish_calc(x_replaced_expr, result);
     free(x_replaced_expr);
     return status;
 }
 
-Point *gen_points(const char *postfixExpression, int x_min, int x_max,
-                  int *num_points) {
-    double total_x_range = x_max - x_min;
+Point *gen_points(char const *const postfixExpression, int const x_min,
+                  int const x_max, int *const num_points) {
+    double const total_x_range = x_max - x_min;
 
-    double x_step = total_x_range / GRAPH_WIDTH;
-    Point *points = (Point *)malloc(GRAPH_WIDTH * sizeof(Point));
+    double const x_step = total_x_range / GRAPH_WIDTH;
+    Point *const points = (Point *)malloc(GRAPH_WIDTH * sizeof(Point));
 
     *num_points = 0;
 
     for (int x_axis = 0; x_axis < GRAPH_WIDTH; x_axis++) {
-        double x_value = x_min + x_axis * x_step;
+        double const x_value = x_min + x_axis * x_step;
         double result = 0.0;
 
         if (evaluate_postfix_expression(postfixExpression, x_value, &result) ==
@@ -58,17 +60,18 @@ Point *gen_points(const char *postfixExpression, int x_min, int x_max,
     return points;
 }
 
-void graph_draw_cns(const char *postfixExpression, int x_min, int x_max,
-                    int y_min, int y_max, int x, int y) {
+void graph_draw_cns(char const *const postfixExpression, int const x_min,
+                    int const x_max, int const y_min, int const y_max,
+                    int const x, int const y) {
     if (x_max == x_min || y_max == y_min) {
         printf(" ");
         return;
     }
 
-    double x_scale = (double)(x_max - x_min) / GRAPH_WIDTH;
-    double y_scale = (double)(y_max - y_min) / GRAPH_HEIGHT;
+    double const x_scale = (double)(x_max - x_min) / GRAPH_WIDTH;
+    double const y_scale = (double)(y_max - y_min) / GRAPH_HEIGHT;
 
-    double x_value = x_min + x * x_scale;
+    double const x_value = x_min + x * x_scale;
     double result = 0.0;
 
     int y_zero = -1;
@@ -93,8 +96,8 @@ void graph_draw_cns(const char *postfixExpression, int x_min, int x_max,
 
     if (evaluate_postfix_expression(postfixExpression, x_value, &result) ==
         ERROR_NONE) {
-        double y_pos = (double)(y_max - result) / y_scale;
-        int y_axis = (int)round(y_pos);
+        double const y_pos = (double)(y_max - result) / y_scale;
+        int const y_axis = (int)round(y_pos);
 
         if (fabs((double)y_axis - y) <= DELTA) {
             ch = '*';
@@ -104,33 +107,35 @@ void graph_draw_cns(const char *postfixExpression, int x_min, int x_max,
     printf("%c", ch);
 }
 
-char *replace_x_with_value(const char *str, double value) {
+char *replace_x_with_value(char const *const str, double const value) {
     if (str == NULL) {
         return NULL;
     }
 
-    int is_negative = value < 0;
+    int const is_negative = value < 0;
 
     char value_str[32];
-    int len_value_str = format_double(value, value_str, sizeof(value_str));
+    int const len_value_str =
+        format_double(value, value_str, sizeof(value_str));
 
     if (len_value_str < 0) {
         return NULL;
     }
 
-    char *replaced_str = replace_character(str, 'x', value_str, is_negative);
+    char *const replaced_str =
+        replace_character(str, 'x', value_str, is_negative);
     return replaced_str;
 }
 
-char *replace_character(const char *src, char target, const char *replacement,
-                        int is_negative) {
+char *replace_character(char const *const src, char const target,
+                        char const *const replacement, int const is_negative) {
     if (src == NULL || replacement == NULL) {
         return NULL;
     }
 
-    int count = count_occurrences(src, target);
+    int const count = count_occurrences(src, target);
     if (count == 0) {
-        char *copy_str = (char *)malloc(strlen(src) + 1);
+        char *const copy_str = (char *)malloc(strlen(src) + 1);
         if (!copy_str) {
             return NULL;
         }
@@ -138,12 +143,12 @@ char *replace_character(const char *src, char target, const char *replacement,
         return copy_str;
     }
 
-    int is_neg = is_negative ? 1 : 0;
-    size_t replacement_length = strlen(replacement);
-    size_t new_length =
+    int const is_neg = is_negative ? 1 : 0;
+    size_t const replacement_length = strlen(replacement);
+    size_t const new_length =
         calculate_new_length(src, count, replacement_length, is_neg);
 
-    char *new_str = (char *)malloc(new_length + 1);
+    char *const new_str = (char *)malloc(new_length + 1);
     if (!new_str) {
         return NULL;
     }
@@ -170,41 +175,42 @@ char *replace_character(const char *src, char target, const char *replacement,
     return new_str;
 }
 
-int count_occurrences(const char *str, char target) {
+size_t calculate_new_length(char const *const str, int const count,
+                            int const replacement_length,
+                            int const is_negative) {
+    if (str == NULL) {
+        return 0;
+    }
+
+    size_t const original_len = strlen(str);
+    size_t const new_len = original_len + count * (replacement_length - 1);
+
+    if (is_negative) {
+        return new_len + count * 2;
+    }
+
+    return new_len;
+}
+
+int format_double(double const value, char *const buffer, size_t const size) {
+    if (buffer == NULL) {
+        return -1;
+    }
+
+    int const len = snprintf(buffer, size, "%.2f", fabs(value));
+    return len;
+}
+
+int count_occurrences(char const *const str, char const target) {
     if (str == NULL) {
         return 0;
     }
 
     int count = 0;
-    for (const char *ptr = str; *ptr != '\0'; ptr++) {
+    for (char const *ptr = str; *ptr != '\0'; ptr++) {
         if (*ptr == target) {
             count++;
         }
     }
     return count;
-}
-
-int format_double(double value, char *buffer, size_t size) {
-    if (buffer == NULL) {
-        return -1;
-    }
-
-    int len = snprintf(buffer, size, "%.2f", fabs(value));
-    return len;
-}
-
-size_t calculate_new_length(const char *str, int count, int replacement_length,
-                            int is_negative) {
-    if (str == NULL) {
-        return 0;
-    }
-
-    size_t original_len = strlen(str);
-    size_t new_len = original_len + count * (replacement_length - 1);
-
-    if (is_negative) {
-        new_len += count * 2;
-    }
-
-    return new_len;
 }
--- src/graph/plot_graph.h
+++ src/graph/plot_graph.h
@@ -18,20 +18,22 @@ typedef struct {
     double y_value;
 } Point;
 
-int plot_graph(const char* postfixExpression);
-int evaluate_postfix_expression(const char* postfixExpression, double x_value,
-                                double* result);
-Point* gen_points(const char* postfixExpression, int x_min, int x_max,
-                  int* num_points);
-void graph_draw_cns(const char* postfixExpression, int x_min, int x_max,
-                    int y_min, int y_max, int x, int y);
+int plot_graph(char const *const postfixExpression);
+int evaluate_postfix_expression(char const *const postfixExpression,
+                                double const x_value, double *const result);
+Point *gen_points(char const *const postfixExpression, int const x_min,
+                  int const x_max, int *const num_points);
+void graph_draw_cns(char const *const postfixExpression, int const x_min,
+                    int const x_max, int const y_min, int const y_max,
+                    int const x, int const y);
 
-char* replace_x_with_value(const char* str, double value);
-char* replace_character(const char* src, char target, const char* replacement,
-                        int is_negative);
-size_t calculate_new_length(const char* str, int count, int replacement_length,
-                            int is_negative);
-int format_double(double value, char* buffer, size_t size);
-int count_occurrences(const char* str, char target);
+char *replace_x_with_value(char const *const str, double const value);
+char *replace_character(char const *const src, char const target,
+                        char const *const replacement, int const is_negative);
+size_t calculate_new_length(char const *const str, int const count,
+                            int const replacement_length,
+                            int const is_negative);
+int format_double(double const value, char *const buffer, size_t const size);
+int count_occurrences(char const *const str, char const target);
 
 #endif  // PLOT_GRAPH_H
