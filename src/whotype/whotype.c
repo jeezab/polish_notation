a70ce0ad2427d8a629a313f13a2cabe64446fd1f
jeezab <zabvenen@yandex.ru>
2024-09-28 07:32:38 +0300

    refactor: added const usage

--- src/whotype/whotype.c
+++ src/whotype/whotype.c
@@ -1,13 +1,13 @@
 #include "whotype.h"
 
-int isDigit(char c) { return c >= '0' && c <= '9'; }
+int isDigit(char const c) { return c >= '0' && c <= '9'; }
 
-int isLetter(char c) {
+int isLetter(char const c) {
     return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
 }
 
-int isSpace(char c) { return c == ' ' || c == '\t' || c == '\n'; }
+int isSpace(char const c) { return c == ' ' || c == '\t' || c == '\n'; }
 
-int isOperator(char c) {
+int isOperator(char const c) {
     return c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '~';
 }
--- src/whotype/whotype.h
+++ src/whotype/whotype.h
@@ -1,10 +1,10 @@
 #ifndef WHOTYPE_H
 #define WHOTYPE_H
 
-int isDigit(char c);
-int isAlpha(char c);
-int isLetter(char c);
-int isSpace(char c);
-int isOperator(char c);
+int isDigit(char const c);
+int isAlpha(char const c);
+int isLetter(char const c);
+int isSpace(char const c);
+int isOperator(char const c);
 
 #endif  // WHOTYPE_H
